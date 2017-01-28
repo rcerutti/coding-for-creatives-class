ba992f9cf28dc9b3f2cf325917f4ebeb638ee833
Ryan Cerutti <RyanCerutti@Ryans-MacBook-Air.local>
2017-01-28 18:55:21 -0500

    updated 2

--- week-4/sketch.js
+++ week-4/sketch.js
@@ -1,12 +1,7 @@
 function setup() {
-  createCanvas(640, 480);
+
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  ellipse(50, 50, 80, 80);
 }
